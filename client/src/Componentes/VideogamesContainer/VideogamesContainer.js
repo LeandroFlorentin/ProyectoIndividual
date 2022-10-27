0f67b3f620447cb0c32b042edfccc06c5dfc5cfb
unknown <leandro.florentin@hotmail.com>
2022-10-27 15:43:29 -0300

    ultimoCambio

--- client/src/Componentes/VideogamesContainer/VideogamesContainer.js
+++ client/src/Componentes/VideogamesContainer/VideogamesContainer.js
@@ -1,13 +1,17 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getVideogames, getGenres } from '../../redux/actions'
 import { useEffect } from 'react';
 import VideoGames from '../Videogames/Videogames.js'
 
 const VideogamesContainer = () => {
     const dispatch = useDispatch()
+    const { videoGames, videoGameActu } = useSelector(state => state)
+
     useEffect(() => {
-        dispatch(getVideogames())
-        dispatch(getGenres())
+        if (videoGames.length === videoGameActu.length) {
+            dispatch(getVideogames())
+            dispatch(getGenres())
+        }
     }, [])
     return (
         <VideoGames />
